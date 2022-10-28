7d37f38cb68eb77c8c0effc6f89e410b04c131b2
Andre Lipke <andre@tst.sh>
2022-10-28 19:40:42 -0400

    Build installer

--- installer/windows/build_installer.ps1
+++ installer/windows/build_installer.ps1
@@ -1,2 +1,2 @@
-$puro_version = ..\..\puro\bin\puro version --plain
+$puro_version = ..\..\puro\bin\puro.exe version --plain
 &"C:\Program Files (x86)\Inno Setup 6\iscc" "/dAppVersion=${puro_version}" install.iss
