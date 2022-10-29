9f99d0d6cc521de33bc0aa70ff6755c9a91a932f
Andre Lipke <andre@tst.sh>
2022-10-28 22:29:52 -0400

    Fix linux builds

--- puro/build.ps1
+++ puro/build.ps1
@@ -1,2 +1,2 @@
 $puro_version = dart bin/puro.dart version --plain @args
-dart compile exe bin/puro.dart --define=puro_version=$puro_version
+dart compile exe bin/puro.dart -o bin/puro.exe --define=puro_version=$puro_version
