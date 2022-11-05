7b46ac6950650dfef314486786b134d11d2dec6e
Andre Lipke <andre@tst.sh>
2022-11-04 21:16:02 -0400

    Add to path

--- puro/lib/src/commands/puro_install.dart
+++ puro/lib/src/commands/puro_install.dart
@@ -23,14 +23,19 @@ class PuroInstallCommand extends PuroCommand {
   Future<CommandResult> run() async {
     final version = await getPuroVersion(scope: scope);
     final config = PuroConfig.of(scope);
-    final profile = await tryUpdateProfile(scope: scope);
     final homeDir = config.homeDir.path;
-    final profilePath = profile?.path.replaceAll(homeDir, '~');
-    final scriptPath = Platform.script.toFilePath().replaceAll(homeDir, '~');
+    final homeVar = Platform.isWindows ? '%HOME%' : '~';
+    final scriptPath =
+        Platform.script.toFilePath().replaceAll(homeDir, homeVar);
+    String? profilePath;
+    if (Platform.isLinux || Platform.isMacOS) {
+      final profile = await tryUpdateProfile(scope: scope);
+      profilePath = profile?.path.replaceAll(homeDir, homeVar);
+    }
     return BasicMessageResult.list(
       success: true,
       messages: [
-        if (profile != null)
+        if (profilePath != null)
           CommandMessage(
             (format) => 'Updated PATH in $profilePath',
           ),
