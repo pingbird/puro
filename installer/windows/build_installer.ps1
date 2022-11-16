54c2f028c27036e367c6d7d5455bd308234c78ce
Andre Lipke <andre@tst.sh>
2022-11-15 21:24:39 -0500

    why are you like this

--- installer/windows/build_installer.ps1
+++ installer/windows/build_installer.ps1
@@ -1,3 +1,4 @@
 $puro_version = ..\..\puro\bin\puro.exe version --plain
 Write-Output "Version: $puro_version"
 &"C:\Program Files (x86)\Inno Setup 6\iscc" "/dAppVersion=${puro_version}" install.iss
+if(!$?) { Exit $LASTEXITCODE }
