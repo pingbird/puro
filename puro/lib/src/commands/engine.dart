b9f14ccc77eb978d45777226a89d80f83ef69550
Andre Lipke <andre@tst.sh>
2022-11-18 21:08:12 -0500

    Bump version

--- puro/lib/src/commands/engine.dart
+++ puro/lib/src/commands/engine.dart
@@ -1,34 +1,9 @@
-import 'dart:io';
-
-import 'package:args/args.dart';
-
 import '../command.dart';
-import '../command_result.dart';
-import '../env/command.dart';
-import '../env/default.dart';
 
-class DartCommand extends PuroCommand {
+class EngineCommand extends PuroCommand {
   @override
-  final name = 'dart';
+  final name = 'engine';
 
   @override
-  final description = 'Forwards arguments to dart in the current environment';
-
-  @override
-  final argParser = ArgParser.allowAnything();
-
-  @override
-  String? get argumentUsage => '[...args]';
-
-  @override
-  Future<CommandResult> run() async {
-    final environment = await getProjectEnvOrDefault(scope: scope);
-    final exitCode = await runDartCommand(
-      scope: scope,
-      environment: environment,
-      args: argResults!.arguments,
-      mode: ProcessStartMode.inheritStdio,
-    );
-    await runner.exitPuro(exitCode);
-  }
+  final description = 'Manages Flutter engine builds';
 }
